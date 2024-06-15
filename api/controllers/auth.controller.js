f1b81d4bab44611b1b07c3dfe56f769d9e5122c3
unknown <oluwatoyosiolaniyan@gmail.com>
2024-06-15 02:06:40 +0100

    create sign up

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
@@ -12,9 +13,19 @@ export const signup = async (req, res) => {
   ) {
     return res.status(400).json({ message: "All fields are required" });
   }
+
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
   const newUser = new User({
     username,
     email,
-    password,
+    password: hashedPassword,
   });
+
+  try {
+    await newUser.save();
+    res.json("Signup successful");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
