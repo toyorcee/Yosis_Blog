93ee86157114fb5817e96802ede0002183fa4d4b
unknown <oluwatoyosiolaniyan@gmail.com>
2024-10-15 20:39:10 +0100

    complete push 15th oct 2024

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,24 +1,24 @@
 export default function About() {
   return (
-    <div className='min-h-screen flex items-center justify-center'>
-      <div className='max-w-2xl mx-auto p-3 text-center'>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
         <div>
-          <h1 className='text-3xl font font-semibold text-center my-7'>
+          <h1 className="text-3xl font font-semibold text-center my-7">
             About Yosi's Blog
           </h1>
-          <div className='text-md text-gray-500 flex flex-col gap-6'>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
             <p>
-              Welcome to Yosi's Blog! This blog was created by Yosi Ghavidel
-              as a personal project to share his thoughts and ideas with the
-              world. Yosi is a passionate developer who loves to write about
-              technology, coding, and everything in between.
+              Welcome to Yosi's Blog! This blog was created by Olaniyan
+              Oluwatoyosi as a personal project to share his thoughts and ideas
+              with the world. Yosi is a passionate developer who loves to write
+              about technology, coding, and everything in between.
             </p>
 
             <p>
               On this blog, you'll find weekly articles and tutorials on topics
               such as web development, software engineering, and programming
-              languages. Yosi is always learning and exploring new
-              technologies, so be sure to check back often for new content!
+              languages. Yosi is always learning and exploring new technologies,
+              so be sure to check back often for new content!
             </p>
 
             <p>
